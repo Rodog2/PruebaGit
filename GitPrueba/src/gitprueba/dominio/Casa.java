ad7a7938bda44f36ecd6d607fad4de32588da12f
Rodri Castillo <rodri_g2@hotmail.com>
2021-10-09 22:57:24 -0300

    cambio definitivo desde master

--- GitPrueba/src/gitprueba/dominio/Casa.java
+++ GitPrueba/src/gitprueba/dominio/Casa.java
@@ -6,14 +6,10 @@ public class Casa {
 
     
     private String casa;
-<<<<<<< HEAD
     private String atributoDesdeCasa;
     private Integer id;
-<<<<<<< HEAD
     private String atributoCreadoDesdeMaster;
-=======
     private String atributoNuevoDesdeCasa
-    
->>>>>>> casa
+
 
 }
